fd40ad6fb500b2a1842c31b3d2c03030419ba5ad
smhrd <smhrd@59.0.234.149>
2022-01-18 11:09:04 +0900

    수진_userInfo

--- src/Model/UserInfoVO.java
+++ src/Model/UserInfoVO.java
@@ -2,4 +2,30 @@ package Model;
 
 public class UserInfoVO {
 
+	// 필드 : 아이디, 비밀번호, 점수
+	private String id;
+	private int password;
+	private int score;
+
+	// 생성자
+	public UserInfoVO(String id, int password, int score) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.score = score;
+	}
+
+	// 메소드
+	public String getID() {
+		return id;
+	}
+
+	public int getPASSWORD() {
+		return password;
+	}
+
+	public int getSCORE() {
+		return score;
+	}
+
 }
